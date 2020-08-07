799477ace2831c2978ef5e470cdf7568bc08d298
Alejandro <aleonskater@gmail.com>
2020-08-06 19:53:26 -0500

    avances de orden de nombres, fet and order

--- index.js
+++ index.js
@@ -13,36 +13,11 @@ const fetchCountHTML = fs.readFileSync(`${__dirname}/fetchcount.html`, 'utf-8');
 const fastestShipHTML = fs.readFileSync(`${__dirname}/fastestship.html`, 'utf-8');
 const planetsByTerrainHTML = fs.readFileSync(`${__dirname}/planetterrain.html`, 'utf-8');
 
-const replaceTemplateFetchOrder = (template, obj) => {
-    let output = template.replace(/{%NOMBRE%}/g, obj.results[0].name.first);
-    output = output.replace(/{%APELLIDO%}/g, obj.results[0].name.last);
-    output = output.replace(/{%EDAD%}/g, obj.results[0].dob.age);
-    output = output.replace(/{%PAIS%}/g, obj.results[0].location.country);
-    output = output.replace(/{%EMAIL%}/g, obj.results[0].email);
-}
-
 app.get('/', (req, res) => {
     res.send(indexHTML);
 });
 
 app.get('/fetch-order', (req, res) => {
-    https.get(apiRandomUser, (res) => {
-
-        let body = '';
-
-        res.on('data', (d) => {
-            body += d;
-        });
-
-        res.on('end', () => {
-            let data = JSON.parse(body);
-            console.log(data.results[0].name.first, data.results[0].name.last);
-        });
-
-    }).on('error', (e) => {
-        console.error(e);
-    });
-
     res.send(fetchOrderHTML);
 });
 
